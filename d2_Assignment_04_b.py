7896f4f9ca49f42d3580b94d119a3d5ef11e968e
SYN2002 <khanra.sayan2002@gmail.com>
2021-09-16 07:44:59 +0530

    first commit

--- d2_Assignment_04_b.py
+++ d2_Assignment_04_b.py
@@ -4,10 +4,11 @@ i,r,rem,sum=0,0,0,0
 while n1>0:
     r=r+1
     n1=n1//10
-for i in range(0,n2):
+for i in range(0,r+1):
     rem=n2%10
     sum = sum+ pow(rem,r)
     n2=n2//10
-print(r)
-print(rem)
-print(sum)
+if(sum==n):
+    print("The given number is an armstrong number!")
+else:
+    print("The given number is not an armstrong number!")
