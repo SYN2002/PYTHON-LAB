05b6645f68b58ce487cdd4594acea5911307d396
SYN2002 <khanra.sayan2002@gmail.com>
2021-09-21 15:16:13 +0530

    first commit

---
+++ d2_Assignment_09_1.py
@@ -0,0 +1,19 @@
+n1=int(input("Enter the lower range: "))
+n2=int(input("Enter the upper range: "))
+
+print("Strong numbers are:")
+
+for i in range(n1,n2+1):
+    n=i
+    s=0
+    r=n
+    for n in range(i,0,-r):
+        j,f=1,1
+        rem=n%10
+        # r=n//10
+        for j in range(1,rem+1):
+            f=f*j
+        s=s+f
+        # n=n//10
+    if(s==i):
+        print(i,end=" ")
